31cd12f58c78120ace496b813ce3a3ee0e597988
tirugit-2001 <tirumaleshaeyadav@gmail.com>
2025-11-10 21:08:09 +0530

    added path

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -12,69 +12,67 @@ import { Toaster } from "sonner";
 function App() {
   return (
     <>
-     <Toaster position="top-right" richColors />
-    <Routes>
-      {/* Public route */}
-      <Route path="/login" element={<LoginPage />} />
+      <Toaster position="top-right" richColors />
+      <Routes>
+        {/* Public route */}
+        <Route path="/login" element={<LoginPage />} />
 
-      {/* Protected routes */}
-      <Route
-        path="/admin"
-        element={
-          <ProtectedRoute>
-            <AdminLayout>
-              <AdminDashboard />
-            </AdminLayout>
-          </ProtectedRoute>
-        }
-      />
+        {/* Protected routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <AdminLayout>
+                <AdminDashboard />
+              </AdminLayout>
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/admin/hero"
-        element={
-          <ProtectedRoute>
-            <AdminLayout>
-              <HeroSectionAdmin />
-            </AdminLayout>
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/admin/hero"
+          element={
+            <ProtectedRoute>
+              <AdminLayout>
+                <HeroSectionAdmin />
+              </AdminLayout>
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/admin/interiorscroll"
-        element={
-          <ProtectedRoute>
-            <AdminLayout>
-              <InteriorScrollAdmin />
-            </AdminLayout>
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/admin/interiorscroll"
+          element={
+            <ProtectedRoute>
+              <AdminLayout>
+                <InteriorScrollAdmin />
+              </AdminLayout>
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/admin/portfolioadmin"
-        element={
-          <ProtectedRoute>
-            <AdminLayout>
-              <PortfolioAdmin />
-            </AdminLayout>
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/admin/portfolioadmin"
+          element={
+            <ProtectedRoute>
+              <AdminLayout>
+                <PortfolioAdmin />
+              </AdminLayout>
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/admin/products"
-        element={
-          <ProtectedRoute>
-            <AdminLayout>
-              <ProductAdmin />
-            </AdminLayout>
-          </ProtectedRoute>
-        }
-      />
-      
-
-    </Routes>
+        <Route
+          path="/admin/products"
+          element={
+            <ProtectedRoute>
+              <AdminLayout>
+                <ProductAdmin />
+              </AdminLayout>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </>
   );
 }
